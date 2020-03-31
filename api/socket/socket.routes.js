7d3fcaf39dc227be793165613e953f1546b6585f
Vlad Indikt <vlad.indikt@gmail.com>
2020-03-31 10:52:32 +0300

    sockets

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -2,9 +2,10 @@ module.exports = connectSockets
 
 function connectSockets(io) {
     io.on('connection', socket => {
+        console.log('connection to socket has been made')
         socket.on('chat newMsg', msg => {
             console.log(msg)
-                // io.emit('chat addMsg', msg)
+                io.emit('chat addMsg', msg) // emit to everyone
                 // emits only to sockets in the same room
             io.to(socket.myTopic).emit('chat addMsg', msg)
         })
